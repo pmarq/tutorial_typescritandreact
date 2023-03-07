87edb9c33b35352bf5a82ffc090dfbdf6c445eb0
DESKTOP-VUTV05S\usuario <paulomuniz2411@gmail.com>
2023-03-07 11:55:27 -0300

    Noteform html and css done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
 import { Routes , Route , Navigate} from "react-router-dom"
+import { NewNote } from "./NewNote"
+
+// my-4 = margin on botton and top
 
 function App() {
-  return(
-   <Routes>
-    <Route path="/" element={<h1>Home</h1>} />
-    <Route path="/new" element={<h1>New</h1>} />
-    <Route path="/:id">
-    <Route index element={<h1>Show</h1>} /> 
-    <Route path="edit" element={<h1>Edit</h1>} />
-    </Route>
-    <Route path= "*" element={<Navigate to="/"/>} />
-  </Routes>
+  return (
+   <Container className="my-4">
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/new" element={<NewNote />} />
+      <Route path="/:id">
+      <Route index element={<h1>Show</h1>} /> 
+      <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path= "*" element={<Navigate to="/"/>} />
+    </Routes>
+  </Container> 
   )
 }
 
