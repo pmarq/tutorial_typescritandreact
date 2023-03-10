717139a5695c72a52881ee418b3bc594b4ab7d90
DESKTOP-VUTV05S\usuario <paulomuniz2411@gmail.com>
2023-03-10 09:47:44 -0300

    tags features and localstorage setup

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,17 +1,21 @@
 import { FormEvent, useRef , useState } from "react"
 import { Form, Stack, Row, Col , Button } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import { Link , useNavigate } from "react-router-dom"
 import CreatableReactSelect from "react-select/creatable"
 import { NoteData , Tag } from "./App"
+import { v4 as uuidV4 } from "uuid"
 
 type NoteFormProps = {
     onSubmit: (data: NoteData) => void;
+    onAddTag: (tag: Tag) => void
+    availableTags: Tag[]
 }
 
-export function NoteForm({ onSubmit }: NoteFormProps){
+export function NoteForm({ onSubmit, onAddTag, availableTags}: NoteFormProps){
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags , setSelectedTags] = useState<Tag[]>([])
+    const navigate = useNavigate()
     
     function handleSubmit(e: FormEvent){
         e.preventDefault()
@@ -19,8 +23,10 @@ export function NoteForm({ onSubmit }: NoteFormProps){
         onSubmit({
             title: titleRef.current!.value,
             markdown: markdownRef.current!.value,
-            tags: []
+            tags: selectedTags
         })
+
+        navigate("..")
     }
 
     return (
@@ -36,15 +42,23 @@ export function NoteForm({ onSubmit }: NoteFormProps){
                     <Col>                
                     <Form.Group controlId="tags">
                     <Form.Label>Tags</Form.Label>
-                    <CreatableReactSelect 
-                    value={selectedTags.map(tag =>{
-                        return{ label: tag.label , value: tag.id }
-                    })} 
-                    onChange={tags => {
-                        setSelectedTags(tags.map(tag =>{
-                            return{ label: tag.label, id: tag.value }
-                        }))
-                    }}
+                    <CreatableReactSelect
+                        onCreateOption={label =>{
+                            const newTag = { id: uuidV4(), label }
+                            onAddTag(newTag)
+                            setSelectedTags(prev =>[...prev, newTag])                            
+                        }} 
+                        value={selectedTags.map(tag =>{
+                            return{ label: tag.label , value: tag.id }
+                        })} 
+                        options={availableTags.map(tag => {
+                            return{ label: tag.label, value: tag.id }
+                        })}
+                        onChange={tags => {
+                            setSelectedTags(tags.map(tag =>{
+                                return{ label: tag.label, id: tag.value }
+                            }))
+                        }}
                     
                     isMulti/>
                     </Form.Group>
